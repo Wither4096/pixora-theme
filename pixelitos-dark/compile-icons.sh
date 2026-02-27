da324e01979b2749fd019d3abdbefc668427fe68
tsora1603 <neiva.cesar07@proton.me>
2026-02-27 00:37:01 -0300

    Added Spotify, Telegram, Brave, Vivaldi, Tor, LibreWolf, OpenRPG and Fluxer icons

--- pixelitos-dark/compile-icons.sh
+++ pixelitos-dark/compile-icons.sh
@@ -19,9 +19,8 @@ if [ ! -d "128" ]; then
 	mkdir -v 128
 fi
 
-
 cp -rvn 16/* 128
 find 128 -type f -name "*.png" -mmin -1 -exec mogrify -resize 128x128! -filter point -verbose {} \; &&
-ln -s ../pixelitos-dark/128 ../pixelitos-light/128
+ln -sfn ../pixelitos-dark/128 ../pixelitos-light/128
 sleep 1 && gtk-update-icon-cache .
 gtk-update-icon-cache  ../pixelitos-light .
